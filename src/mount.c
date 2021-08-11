3651c41b233e5df437734bace287b2a4a516600c
Smart6502 <69001578+Smart6502@users.noreply.github.com>
2021-08-11 21:12:42 +0530

    pls work

--- src/mount.c
+++ src/mount.c
@@ -1,14 +1,27 @@
+#include <stdint.h>
 #include <sys/mount.h>
 #include "common.h"
 
 void mount_fss()
 {
-	if (!(mount("none", "/sys", "sysfs", 0, "")))
+	uint64_t prsflags = MS_SYNC | MS_NOSUID | MS_NOEXEC;
+
+	dlog(info, "Mounting /proc...");
+	if (!(mount("proc", "/proc", "procfs", prsflags, "")))
+	{
+		dlog(fail, "Mounting /proc failed");
+	}
+
+	dlog(info, "Mounting /sys...");
+	if (!(mount("sys", "/sys", "sysfs", prsflags, "")))
 	{
 		dlog(fail, "Mounting /sys failed!");
 	}
 	
-	if (!(mount("none", "/proc", "procfs", 0, "")))
+	uint64_t runflags = MS_SYNC | MS_NOSUID;
+
+	dlog(info, "Mounting /run...");
+	if (!(mount("run", "/run", "tmpfs", runflags, "mode=0755")))
 	{
 		dlog(fail, "Mounting /proc failed");
 	}
