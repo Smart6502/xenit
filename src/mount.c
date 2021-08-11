cc952819c1e7667b7d56837e0ba93084699c0a92
Smart6502 <69001578+Smart6502@users.noreply.github.com>
2021-08-11 21:30:08 +0530

    Ok messages for mount

--- src/mount.c
+++ src/mount.c
@@ -4,25 +4,27 @@
 
 void mount_fss()
 {
-	uint64_t prsflags = MS_SYNC | MS_NOSUID | MS_NOEXEC;
+	uint64_t prsflags = MS_SYNCHRONOUS | MS_NOSUID | MS_NOEXEC;
 
 	dlog(info, "Mounting /proc...");
 	if (!(mount("proc", "/proc", "procfs", prsflags, "")))
-	{
 		dlog(fail, "Mounting /proc failed");
-	}
+	else
+		dlog(ok, "Mounted /proc");
+
 
 	dlog(info, "Mounting /sys...");
 	if (!(mount("sys", "/sys", "sysfs", prsflags, "")))
-	{
 		dlog(fail, "Mounting /sys failed!");
-	}
-	
+	else
+		dlog(ok, "Mounted /sys");
+
+
 	uint64_t runflags = MS_SYNC | MS_NOSUID;
 
 	dlog(info, "Mounting /run...");
 	if (!(mount("run", "/run", "tmpfs", runflags, "mode=0755")))
-	{
-		dlog(fail, "Mounting /proc failed");
-	}
+		dlog(fail, "Mounting /run failed");
+	else
+		dlog(ok, "Mounted /run");
 }
