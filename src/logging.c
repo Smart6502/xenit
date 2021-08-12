c5d93ef2a2db70ab060610125ade1abf58ea3090
Smart6502 <69001578+Smart6502@users.noreply.github.com>
2021-08-12 16:27:05 +0530

    LOG

--- src/logging.c
+++ src/logging.c
@@ -5,22 +5,22 @@ void dlog_sym(int level)
 	switch (level)
 	{
 		case info:
-			fputs("[\e[1;34mi\e[0m]: ", stdout);
+			fputs("\e[1;34m[i]\e[0m: ", stdout);
 			break;
 		case ok:
-			fputs("[\e[1;32m>\e[0m]: ", stdout);
+			fputs("\e[1;32m[>]\e[0m: ", stdout);
 			break;
 		case warn:
-			fputs("[\e[1;33m!\e[0m]: ", stdout);
+			fputs("\e[1;33m[!]\e[0m: ", stdout);
 			break;
 		case fail:
-			fputs("[\e[1;31me\e[0m]: ", stdout);
+			fputs("\e[1;31m[e]\e[0m: ", stdout);
 			break;
 		case fatal:
-			fputs("[\e[1;31mx\e[0m]: ", stdout);
+			fputs("\e[1;31m[x]\e[0m: ", stdout);
 			break;
 		default:
-			fputs("[\e[1;37m?\e[0m]: ", stdout);
+			fputs("\e[1;37m[?]\e[0m: ", stdout);
 			break;
 	}
 }
