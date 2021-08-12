98db76254323a2ff017dd4c37cfb6921a2dc16f1
Smart6502 <69001578+Smart6502@users.noreply.github.com>
2021-08-12 08:11:28 +0530

    removed fstab for now

--- src/mount.c
+++ src/mount.c
@@ -1,32 +1,6 @@
 #include <stdint.h>
-#include <stdlib.h>
 #include <sys/mount.h>
 #include "common.h"
-
-void mount_fstab_fss()
-{
-	if (access("/etc/fstab", F_OK))
-	{
-		dlog(fail, "Could not read from /etc/fstab, it does not exist!");
-		return;
-	}
-
-	FILE *fstab = fopen("/etc/fstab", "r");
-	if (fstab == NULL)
-	{
-		dlog(fail, "Could not open /etc/fstab");
-	}
-
-	fseek(fstab, 0, SEEK_END);
-	size = ftell(fstab);
-	fseek(fstab, 0, SEEK_SET);
-
-	fstab_d = calloc(size + 1, 1);
-	fread(fstab_d, 1, size, fstab);	
-
-	free(fstab_d);
-	fclose(fstab);
-}
 
 void mount_fss()
 {
@@ -61,6 +35,4 @@ void mount_fss()
 	{
 		dlog(ok, "Mounted /dev");
 	}
-
-	mount_fstab_fss();
 }
