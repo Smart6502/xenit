192a155911bcbddab6375658caf11ac3446bfc29
Smart6502 <69001578+Smart6502@users.noreply.github.com>
2021-08-12 08:08:28 +0530

    Using symbols now

--- src/logging.c
+++ src/logging.c
@@ -5,19 +5,19 @@ void dlog_sym(int level)
 	switch (level)
 	{
 		case fail:
-			printf("\e[1;31m  fail  \e[0m =>");
+			printf("[\e[1;31mx\e[0m] =>");
 			break;
 		case fatal:
-			printf("\e[1;31m  fatal \e[0m =>");
+			printf("[\e[1;31mX\e[0m] =>");
 			break;
 		case ok:
-			printf("\e[1;32m   ok   \e[0m =>");
+			printf("[\e[1;32m>\e[0m] =>");
 			break;
 		case warn:
-			printf("\e[1;33m  warn  \e[0m =>");
+			printf("[\e[1;33m!\e[0m] =>");
 			break;
 		case info:
-			printf("\e[1;34m  info  \e[0m =>");
+			printf("[\e[1;34mi\e[0m] =>");
 			break;
 	}
 }
