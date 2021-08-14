10b60d9f90791597141472e33adc8e8155a03f6a
Smart6502 <69001578+Smart6502@users.noreply.github.com>
2021-08-14 23:15:22 +0530

    Reworking on init bruh

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-objs=" "
-
-for i in $(find src/ -type f -name '*.c')
-do
-	echo "Compiling $i"
-	gcc -o ${i/.*}.o -c $i -O2 -Wall -Wextra -Werror
-done
-
-gcc -o xenit $(find src/ -type f -name '*.o') -static -O2
+gcc -o xenit xenit.c -static -O2
